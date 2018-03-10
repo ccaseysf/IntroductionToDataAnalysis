da9ae12cb6963501a3e1cdc482203538bed93ab4
Christine Casey <32369127+ccaseysf@users.noreply.github.com>
2018-03-09 16:15:30 -0800

    Fixed #1 - Simplifying expressions with booleans

--- biodiversity - final.py
+++ biodiversity - final.py
@@ -324,7 +324,7 @@ str2 = 'This string contains Cows'
 # In[25]:
 
 
-species['is_sheep'] = species.common_names.apply(lambda x: True if 'Sheep' in x else False)
+species['is_sheep'] = species.common_names.apply(lambda x: 'Sheep' in x)
 species.head()
 
 
@@ -333,7 +333,7 @@ species.head()
 # In[26]:
 
 
-species[species.is_sheep == True]
+species[species.is_sheep]
 
 
 # Many of the results are actually plants.  Select the rows of `species` where `is_sheep` is `True` and `category` is `Mammal`.  Save the results to the variable `sheep_species`.
